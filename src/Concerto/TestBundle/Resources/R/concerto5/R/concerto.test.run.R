campsych/concerto-platform

commit 49acd308cb7dc5e71dba6f959c6aad71e6ba099f

node input ports can now accept multiple connections

src/Concerto/TestBundle/Resources/R/concerto5/DESCRIPTION
@@ -1,8 +1,8 @@
 Package: concerto5
 Type: Package
 Title: Package for Concerto platform test logic R code.
-Version: 0.7.6
-Date: 2016-05-09
+Version: 0.8.0
+Date: 2016-05-11
 Author: Przemyslaw Lis
 Maintainer: Przemyslaw Lis <pl362@cam.ac.uk>
 Description: Contains all required functions by Concerto platform. 

src/Concerto/TestBundle/Resources/R/concerto5/R/concerto.test.run.R
@@ -102,20 +102,19 @@ concerto.test.run <-
           for (port_id in ls(concerto$flow[[flowIndex]]$ports)){
             port = concerto$flow[[flowIndex]]$ports[[as.character(port_id)]]
             if(port$node_id == node$id && port$type == input_type) {
-              if(port$string == 0){
-                node_params[[port$name]] = eval(parse(text=port$value))
-              } else {
-                node_params[[port$name]] = port$value
-              }
-              
+              port_connected = FALSE
               for (connection_id in ls(concerto$flow[[flowIndex]]$connections)){
                 connection = concerto$flow[[flowIndex]]$connections[[as.character(connection_id)]]
                 if(!is.na(connection$destinationPort_id) && connection$destinationPort_id == port$id) {
-                  func = paste("retFunc = function(",concerto$flow[[flowIndex]]$ports[[as.character(connection$sourcePort_id)]]$name,"){ ",connection$returnFunction," }",sep="")
-                  eval(parse(text=func))
-                  node_params[[port$name]] = retFunc(concerto$flow[[flowIndex]]$ports[[as.character(connection$sourcePort_id)]]$value)
+                  port_connected = TRUE
                   break
                 }
+              }
+
+              if(port$string == 0 && !port_connected){
+                node_params[[port$name]] = eval(parse(text=port$value))
+              } else {
+                node_params[[port$name]] = port$value
               }
             }
           }
@@ -179,7 +178,30 @@ concerto.test.run <-
             }
           }
         }
+
+        #values connections
+        for (source_port_id in ls(concerto$flow[[flowIndex]]$ports)){
+            source_port = concerto$flow[[flowIndex]]$ports[[source_port_id]]
+            if(source_port$node_id == node$id && source_port$type == 1) {
+
+              for (connection_id in ls(concerto$flow[[flowIndex]]$connections)){
+                connection = concerto$flow[[flowIndex]]$connections[[as.character(connection_id)]]
+                if(connection$sourcePort_id == source_port$id) {
+                    func = paste("retFunc = function(",concerto$flow[[flowIndex]]$ports[[as.character(connection$sourcePort_id)]]$name,"){ ",connection$returnFunction," }",sep="")
+                    eval(parse(text=func))
+
+                    for (dest_port_id in ls(concerto$flow[[flowIndex]]$ports)){
+                        dest_port = concerto$flow[[flowIndex]]$ports[[dest_port_id]]
+                        if(connection$destinationPort_id == dest_port$id && dest_port$node_id == connection$destinationNode_id && dest_port$type == 0) {
+                            concerto$flow[[flowIndex]]$ports[[dest_port_id]]$value = retFunc(concerto$flow[[flowIndex]]$ports[[as.character(connection$sourcePort_id)]]$value)
+                        }
+                    }
+                }
+              }
+            }
+        }
       }
+
       concerto$flow[[flowIndex]] <<- NULL
     }
     

src/Concerto/TestBundle/Resources/R/concerto5/man/concerto-package.Rd
@@ -11,8 +11,8 @@ Contains all required functions by Concerto platform.
 \tabular{ll}{
 Package: \tab concerto5\cr
 Type: \tab Package\cr
-Version: \tab 0.7.6\cr
-Date: \tab 2016-05-09\cr
+Version: \tab 0.8.0\cr
+Date: \tab 2016-05-11\cr
 License: \tab GNU GPL v2\cr
 }
 

src/Concerto/PanelBundle/Resources/public/angularjs/app/concerto_panel/js/directives/flow_logic_directive.js
@@ -201,6 +201,7 @@ angular.module('concertoPanel').directive('flowLogic', ['$http', '$compile', '$t
                         if (scope.isPortVisible(node, port) && ((node.type === 0 && port.variableObject.type === 0) || (node.type === 2 && port.variableObject.type === 1))) { //input param
                             jsPlumb.addEndpoint(elem, {
                                 uuid: "node" + node.id + "-ep" + port.id,
+                                maxConnections: -1,
                                 isTarget: true,
                                 endpoint: "Dot",
                                 anchor: [0, 0, -1, 0, 0, portTopMargin + leftCount * portElemMargin],

commit b7c73d6503b33166cdf68d065ff40008b7718c66

fixed flowchart infinite loop regression from 49acd308cb7dc5e71dba6f959c6aad71e6ba099f

app/config/config.yml
@@ -1,5 +1,5 @@
 parameters:
-    version: 5.0.beta.2.101
+    version: 5.0.beta.2.102
     available_locales:
         en_GB: english
         pl_PL: polski

src/Concerto/TestBundle/Resources/R/concerto5/DESCRIPTION
@@ -1,8 +1,8 @@
 Package: concerto5
 Type: Package
 Title: Package for Concerto platform test logic R code.
-Version: 0.8.0
-Date: 2016-05-11
+Version: 0.8.1
+Date: 2016-05-13
 Author: Przemyslaw Lis
 Maintainer: Przemyslaw Lis <pl362@cam.ac.uk>
 Description: Contains all required functions by Concerto platform. 

src/Concerto/TestBundle/Resources/R/concerto5/R/concerto.test.run.R
@@ -180,25 +180,14 @@ concerto.test.run <-
         }
 
         #values connections
-        for (source_port_id in ls(concerto$flow[[flowIndex]]$ports)){
-            source_port = concerto$flow[[flowIndex]]$ports[[source_port_id]]
-            if(source_port$node_id == node$id && source_port$type == 1) {
-
-              for (connection_id in ls(concerto$flow[[flowIndex]]$connections)){
-                connection = concerto$flow[[flowIndex]]$connections[[as.character(connection_id)]]
-                if(connection$sourcePort_id == source_port$id) {
-                    func = paste("retFunc = function(",concerto$flow[[flowIndex]]$ports[[as.character(connection$sourcePort_id)]]$name,"){ ",connection$returnFunction," }",sep="")
-                    eval(parse(text=func))
-
-                    for (dest_port_id in ls(concerto$flow[[flowIndex]]$ports)){
-                        dest_port = concerto$flow[[flowIndex]]$ports[[dest_port_id]]
-                        if(connection$destinationPort_id == dest_port$id && dest_port$node_id == connection$destinationNode_id && dest_port$type == 0) {
-                            concerto$flow[[flowIndex]]$ports[[dest_port_id]]$value = retFunc(concerto$flow[[flowIndex]]$ports[[as.character(connection$sourcePort_id)]]$value)
-                        }
-                    }
-                }
-              }
-            }
+        for (connection_id in ls(concerto$flow[[flowIndex]]$connections)){
+            connection = concerto$flow[[flowIndex]]$connections[[as.character(connection_id)]]
+            if(connection$sourceNode_id != node$id) { next }
+            if(concerto$flow[[flowIndex]]$ports[[as.character(connection$sourcePort_id)]]$type != 1) { next }
+            
+            func = paste("retFunc = function(",concerto$flow[[flowIndex]]$ports[[as.character(connection$sourcePort_id)]]$name,"){ ",connection$returnFunction," }",sep="")
+            eval(parse(text=func))
+            concerto$flow[[flowIndex]]$ports[[as.character(connection$destinationPort_id)]]$value <<- retFunc(concerto$flow[[flowIndex]]$ports[[as.character(connection$sourcePort_id)]]$value)
         }
       }
 

src/Concerto/TestBundle/Resources/R/concerto5/man/concerto-package.Rd
@@ -11,8 +11,8 @@ Contains all required functions by Concerto platform.
 \tabular{ll}{
 Package: \tab concerto5\cr
 Type: \tab Package\cr
-Version: \tab 0.8.0\cr
-Date: \tab 2016-05-11\cr
+Version: \tab 0.8.1\cr
+Date: \tab 2016-05-13\cr
 License: \tab GNU GPL v2\cr
 }
 