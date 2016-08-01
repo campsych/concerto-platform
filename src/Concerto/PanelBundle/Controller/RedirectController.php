214e368b6be8f8ef8bfe43333307876af3c88cc4
Przemysław Lis <przemyslawlis@parabola-interactive.com>
2016-08-01 16:46:01 +0200

    redirect from GET URLs with trailing slashes

---
+++ src/Concerto/PanelBundle/Controller/RedirectController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Concerto\PanelBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class RedirectController extends Controller {
+
+    public function removeTrailingSlashAction(Request $request) {
+        $pathInfo = $request->getPathInfo();
+        $requestUri = $request->getRequestUri();
+
+        $url = str_replace($pathInfo, rtrim($pathInfo, ' /'), $requestUri);
+
+        return $this->redirect($url, 301);
+    }
+
+}
