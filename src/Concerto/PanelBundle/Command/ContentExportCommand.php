328bb80bd1671f73125f6afdffdcc35361c74ee9
Przemysław Lis <przemyslawlis@parabola-interactive.com>
2016-09-06 19:16:11 +0200

    fixed content:export command not working without --set-protected option added missing 'protected' flags to starter content

--- src/Concerto/PanelBundle/Command/ContentExportCommand.php
+++ src/Concerto/PanelBundle/Command/ContentExportCommand.php
@@ -30,12 +30,12 @@ class ContentExportCommand extends ContainerAwareCommand {
             "TestWizard",
             "ViewTemplate"
         );
-        $importService = $this->getContainer()->get('concerto_panel.import_service');
         $em = $this->getContainer()->get("doctrine")->getManager();
         foreach ($classes as $class_name) {
-            $service = $importService->serviceMap[$class_name];
             $repo = $em->getRepository("ConcertoPanelBundle:" . $class_name);
             $collection = $repo->findBy(array("starterContent" => 1));
+            $importService = $this->getContainer()->get('concerto_panel.import_service');
+            $service = $importService->serviceMap[$class_name];
             foreach ($collection as $ent) {
                 $ent = $service->get($ent->getId(), false, false);
                 $arr = $service->entityToArray($ent);
@@ -58,7 +58,7 @@ class ContentExportCommand extends ContainerAwareCommand {
         $file_path = $path . $class_name . "_" . $name . ".concerto.json";
         $fs->dumpFile($file_path, $content);
     }
-    
+
     protected function protectStarterContent(InputInterface $input, OutputInterface $output) {
         $output->writeln("setting starter content as protected");
         $classes = array(
