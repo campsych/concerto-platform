b6f020473ef698242eaa95b10a321ab690183921
Przemysław Lis <przemyslawlis@parabola-interactive.com>
2016-09-27 13:15:16 +0200

    html param in concerto.template.show works now

--- src/Concerto/TestBundle/Resources/R/concerto5/R/concerto.template.show.R
+++ src/Concerto/TestBundle/Resources/R/concerto5/R/concerto.template.show.R
@@ -8,17 +8,17 @@ concerto.template.show = function(
     if(!is.list(params)) stop("'params' must be a list!")
   
     if(templateId==-1 && html=="") stop("templateId or html must be declared")
-    
-    template <- concerto.template.get(templateId)
+   
     if(html!=""){
       concerto$session$templateHead <<- concerto.template.insertParams(head,params)
       concerto$session$templateHtml <<- concerto.template.insertParams(html,params)
     } else {
+      template <- concerto.template.get(templateId)
       if(dim(template)[1]==0) stop(paste("Template #",templateId," not found!",sep=''))
       concerto$session$templateHead <<- concerto.template.insertParams(template$head,params)
       concerto$session$templateHtml <<- concerto.template.insertParams(template$html,params)
+      concerto$session$template_id <<- template$id
     }
-    concerto$session$template_id <<- template$id
     concerto$session$timeLimit <<- timeLimit
 
     if(finalize) {
