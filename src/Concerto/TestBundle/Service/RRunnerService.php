326fe0ccd8f40195091f4a6f78dcfeea92c57fbb
Przemyslaw Lis <przemyslawlis@parabola-interactive.com>
2017-07-31 00:12:23 +0200

    memory optimizations

--- src/Concerto/TestBundle/Service/RRunnerService.php
+++ src/Concerto/TestBundle/Service/RRunnerService.php
@@ -10,7 +10,8 @@ use Concerto\TestBundle\Service\TestSessionCountService;
 use Psr\Log\LoggerInterface;
 use Concerto\PanelBundle\Service\LoadBalancerInterface;
 
-class RRunnerService {
+class RRunnerService
+{
 
     const OS_WIN = 0;
     const OS_LINUX = 1;
@@ -23,8 +24,10 @@ class RRunnerService {
     private $administrationService;
     private $testSessionCountService;
     private $loadBalancerService;
+    private $environment;
 
-    public function __construct($root, $panelNodes, $settings, Registry $doctrine, LoggerInterface $logger, AdministrationService $administrationService, TestSessionCountService $testSessionCountService, LoadBalancerInterface $loadBalancerService) {
+    public function __construct($root, $panelNodes, $settings, Registry $doctrine, LoggerInterface $logger, AdministrationService $administrationService, TestSessionCountService $testSessionCountService, LoadBalancerInterface $loadBalancerService, $environment)
+    {
         $this->root = $root;
         $this->panelNodes = $panelNodes;
         $this->settings = $settings;
@@ -33,9 +36,11 @@ class RRunnerService {
         $this->administrationService = $administrationService;
         $this->testSessionCountService = $testSessionCountService;
         $this->loadBalancerService = $loadBalancerService;
+        $this->environment = $environment;
     }
 
-    private function authorizePanelNode($node_ip, $node_hash) {
+    private function authorizePanelNode($node_ip, $node_hash)
+    {
         foreach ($this->panelNodes as $node) {
             if ($node_hash == $node["hash"]) {
                 return $node;
@@ -44,7 +49,8 @@ class RRunnerService {
         return false;
     }
 
-    public function startR($panel_node_hash, $panel_node_port, $session_hash, $values, $client_ip, $client_browser, $calling_node_ip, $debug) {
+    public function startR($panel_node_hash, $panel_node_port, $session_hash, $values, $client_ip, $client_browser, $calling_node_ip, $debug)
+    {
         $this->logger->info(__CLASS__ . ":" . __FUNCTION__ . " - $panel_node_hash, $panel_node_port, $session_hash, $values, $client_ip, $client_browser, $debug");
 
         $response = array("source" => TestSessionService::SOURCE_PROCESS, "code" => TestSessionService::RESPONSE_STARTING);
@@ -71,7 +77,8 @@ class RRunnerService {
         return $response;
     }
 
-    private function getSerializedConnection($node) {
+    private function getSerializedConnection($node)
+    {
         $con = $this->doctrine->getConnection($node["connection"]);
         $con_array = array(
             "driver" => $con->getDriver()->getName(),
@@ -97,7 +104,8 @@ class RRunnerService {
     }
 
     //@TODO proper OS detection
-    public function getOS() {
+    public function getOS()
+    {
         if (strpos(strtolower(PHP_OS), "win") !== false) {
             return self::OS_WIN;
         } else {
@@ -105,23 +113,28 @@ class RRunnerService {
         }
     }
 
-    public function getIniFilePath() {
+    public function getIniFilePath()
+    {
         return $this->root . "/../src/Concerto/TestBundle/Resources/R/initialization.R";
     }
 
-    private function getOutputFilePath($node_id, $session_hash) {
+    private function getOutputFilePath($node_id, $session_hash)
+    {
         return $this->getWorkingDirPath($node_id, $session_hash) . "concerto.log";
     }
 
-    private function getPublicDirPath() {
+    private function getPublicDirPath()
+    {
         return $this->root . "/../src/Concerto/PanelBundle/Resources/public/files/";
     }
 
-    private function getMediaUrl($node) {
+    private function getMediaUrl($node)
+    {
         return $node["dir"] . "bundles/concertopanel/files/";
     }
 
-    private function getWorkingDirPath($node_id, $session_hash) {
+    private function getWorkingDirPath($node_id, $session_hash)
+    {
         $path = $this->root . "/../src/Concerto/TestBundle/Resources/sessions/$node_id/$session_hash/";
         if (!file_exists($path)) {
             mkdir($path, 0755, true);
@@ -129,7 +142,8 @@ class RRunnerService {
         return $path;
     }
 
-    private function escapeWindowsArg($arg) {
+    private function escapeWindowsArg($arg)
+    {
         $arg = addcslashes($arg, '"');
         $arg = str_replace("(", "^(", $arg);
         $arg = str_replace(")", "^)", $arg);
@@ -137,7 +151,8 @@ class RRunnerService {
     }
 
     //@TODO must not send plain password through command line
-    private function getCommand($panel_node, $panel_node_connection, $client, $session_hash, $values, $debug) {
+    private function getCommand($panel_node, $panel_node_connection, $client, $session_hash, $values, $debug)
+    {
         $this->logger->info(__CLASS__ . ":" . __FUNCTION__ . " - $panel_node, $panel_node_connection, $client, $session_hash, $values, $debug");
         $max_idle_time = $this->settings["max_idle_time"];
         $max_exec_time = $this->settings["max_execution_time"];
@@ -154,58 +169,59 @@ class RRunnerService {
         switch ($this->getOS()) {
             case self::OS_WIN:
                 return "start cmd /C \""
-                        . "\"" . $this->escapeWindowsArg($this->settings["php_exec"]) . "\" "
-                        . "\"" . $this->escapeWindowsArg($this->root) . DIRECTORY_SEPARATOR . "console\" concerto:r:start "
-                        . "\"" . $this->escapeWindowsArg($this->settings["rscript_exec"]) . "\" "
-                        . "\"" . $this->escapeWindowsArg($this->getIniFilePath()) . "\" "
-                        . "\"" . $this->escapeWindowsArg($test_node) . "\" "
-                        . "\"" . $this->escapeWindowsArg($panel_node) . "\" "
-                        . "$session_hash "
-                        . "\"" . $this->escapeWindowsArg($panel_node_connection) . "\" "
-                        . "\"" . $this->escapeWindowsArg($client) . "\" "
-                        . "\"" . $this->escapeWindowsArg($this->getWorkingDirPath($decoded_panel_node["id"], $session_hash)) . "\" "
-                        . "\"" . $this->escapeWindowsArg($this->getPublicDirPath()) . "\" "
-                        . "\"" . $this->escapeWindowsArg($this->getMediaUrl($decoded_test_node)) . "\" "
-                        . "\"" . $this->escapeWindowsArg($this->getOutputFilePath($decoded_panel_node["id"], $session_hash)) . "\" "
-                        . "$debug "
-                        . "$max_idle_time "
-                        . "$max_exec_time "
-                        . "$keep_alive_interval_time "
-                        . "$keep_alive_tolerance_time "
-                        . "\"" . ($values ? $this->escapeWindowsArg($values) : "{}") . "\" "
-                        . "$renviron "
-                        . ">> "
-                        . "\"" . $this->escapeWindowsArg($this->getOutputFilePath($decoded_panel_node["id"], $session_hash)) . "\" "
-                        . "2>&1\"";
+                    . "\"" . $this->escapeWindowsArg($this->settings["php_exec"]) . "\" "
+                    . "\"" . $this->escapeWindowsArg($this->root) . DIRECTORY_SEPARATOR . "console\" concerto:r:start --env=" . $this->environment . " "
+                    . "\"" . $this->escapeWindowsArg($this->settings["rscript_exec"]) . "\" "
+                    . "\"" . $this->escapeWindowsArg($this->getIniFilePath()) . "\" "
+                    . "\"" . $this->escapeWindowsArg($test_node) . "\" "
+                    . "\"" . $this->escapeWindowsArg($panel_node) . "\" "
+                    . "$session_hash "
+                    . "\"" . $this->escapeWindowsArg($panel_node_connection) . "\" "
+                    . "\"" . $this->escapeWindowsArg($client) . "\" "
+                    . "\"" . $this->escapeWindowsArg($this->getWorkingDirPath($decoded_panel_node["id"], $session_hash)) . "\" "
+                    . "\"" . $this->escapeWindowsArg($this->getPublicDirPath()) . "\" "
+                    . "\"" . $this->escapeWindowsArg($this->getMediaUrl($decoded_test_node)) . "\" "
+                    . "\"" . $this->escapeWindowsArg($this->getOutputFilePath($decoded_panel_node["id"], $session_hash)) . "\" "
+                    . "$debug "
+                    . "$max_idle_time "
+                    . "$max_exec_time "
+                    . "$keep_alive_interval_time "
+                    . "$keep_alive_tolerance_time "
+                    . "\"" . ($values ? $this->escapeWindowsArg($values) : "{}") . "\" "
+                    . "$renviron "
+                    . ">> "
+                    . "\"" . $this->escapeWindowsArg($this->getOutputFilePath($decoded_panel_node["id"], $session_hash)) . "\" "
+                    . "2>&1\"";
             default:
                 return "nohup "
-                        . $this->settings["php_exec"] . " "
-                        . "'" . $this->root . "/console' concerto:r:start "
-                        . "'" . $this->settings["rscript_exec"] . "' "
-                        . "'" . $this->getIniFilePath() . "' "
-                        . "'$test_node' "
-                        . "'$panel_node' "
-                        . "$session_hash "
-                        . "'$panel_node_connection' "
-                        . "'$client' "
-                        . "'" . $this->getWorkingDirPath($decoded_panel_node["id"], $session_hash) . "' "
-                        . "'" . $this->getPublicDirPath() . "' "
-                        . "'" . $this->getMediaUrl($decoded_test_node) . "' "
-                        . "'" . $this->getOutputFilePath($decoded_panel_node["id"], $session_hash) . "' "
-                        . "$debug "
-                        . "$max_idle_time "
-                        . "$max_exec_time "
-                        . "$keep_alive_interval_time "
-                        . "$keep_alive_tolerance_time "
-                        . ($values ? "'" . $values . "'" : "'{}'") . " "
-                        . "$renviron "
-                        . ">> "
-                        . $this->getOutputFilePath($decoded_panel_node["id"], $session_hash) . " "
-                        . "2>&1 & echo $!";
+                    . $this->settings["php_exec"] . " "
+                    . "'" . $this->root . "/console' concerto:r:start --env=" . $this->environment . " "
+                    . "'" . $this->settings["rscript_exec"] . "' "
+                    . "'" . $this->getIniFilePath() . "' "
+                    . "'$test_node' "
+                    . "'$panel_node' "
+                    . "$session_hash "
+                    . "'$panel_node_connection' "
+                    . "'$client' "
+                    . "'" . $this->getWorkingDirPath($decoded_panel_node["id"], $session_hash) . "' "
+                    . "'" . $this->getPublicDirPath() . "' "
+                    . "'" . $this->getMediaUrl($decoded_test_node) . "' "
+                    . "'" . $this->getOutputFilePath($decoded_panel_node["id"], $session_hash) . "' "
+                    . "$debug "
+                    . "$max_idle_time "
+                    . "$max_exec_time "
+                    . "$keep_alive_interval_time "
+                    . "$keep_alive_tolerance_time "
+                    . ($values ? "'" . $values . "'" : "'{}'") . " "
+                    . "$renviron "
+                    . ">> "
+                    . $this->getOutputFilePath($decoded_panel_node["id"], $session_hash) . " "
+                    . "2>&1 & echo $!";
         }
     }
 
-    private function startProcess($panel_node, $panel_node_connection, $client, $session_hash, $values, $debug) {
+    private function startProcess($panel_node, $panel_node_connection, $client, $session_hash, $values, $debug)
+    {
         $this->logger->info(__CLASS__ . ":" . __FUNCTION__ . " - $panel_node, $panel_node_connection, $client, $session_hash, $values, $debug");
         $command = $this->getCommand($panel_node, $panel_node_connection, $client, $session_hash, $values, $debug);
         $this->logger->info(__CLASS__ . ":" . __FUNCTION__ . ":command: $command");
@@ -221,11 +237,13 @@ class RRunnerService {
         return $process->getExitCode();
     }
 
-    public function getUploadDirectory() {
+    public function getUploadDirectory()
+    {
         return dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'PanelBundle' . DIRECTORY_SEPARATOR . ($this->environment === "test" ? ("Tests" . DIRECTORY_SEPARATOR) : "") . "Resources" . DIRECTORY_SEPARATOR . "public" . DIRECTORY_SEPARATOR . "files" . DIRECTORY_SEPARATOR;
     }
 
-    public function uploadFile($session_hash, $calling_node_ip, $files, $name) {
+    public function uploadFile($session_hash, $calling_node_ip, $files, $name)
+    {
         $this->logger->info(__CLASS__ . ":" . __FUNCTION__ . " - $session_hash, $calling_node_ip, $name");
 
         $response = array("result" => -1);
