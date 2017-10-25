72e940955444b63d63b991dd94a5f5e35cf03079
Przemyslaw Lis <przemyslawlis@parabola-interactive.com>
2017-10-25 15:44:34 +0200

    flow entities

--- src/Concerto/PanelBundle/Entity/TestNodeConnection.php
+++ src/Concerto/PanelBundle/Entity/TestNodeConnection.php
@@ -25,11 +25,13 @@ class TestNodeConnection extends AEntity implements \JsonSerializable {
 
     /**
      * @ORM\ManyToOne(targetEntity="TestNodePort", inversedBy="sourceForConnections")
+     * @ORM\JoinColumn(onDelete="CASCADE")
      */
     private $sourcePort;
 
     /**
      * @ORM\ManyToOne(targetEntity="TestNode", inversedBy="destinationForConnections")
+     * @ORM\JoinColumn(onDelete="CASCADE")
      */
     private $destinationNode;
 
