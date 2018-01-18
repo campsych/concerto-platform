0c17e4dc1c4aa1a873c31e638f12f09d636b7e74
Przemyslaw Lis <przemyslawlis@parabola-interactive.com>
2018-01-18 18:13:28 +0100

    update to symfony 3.1

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,9 +18,10 @@ class AppKernel extends Kernel
 
             new FOS\OAuthServerBundle\FOSOAuthServerBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
+            new Cocur\Slugify\Bridge\Symfony\CocurSlugifyBundle(),
+
             new Concerto\PanelBundle\ConcertoPanelBundle(),
             new Concerto\TestBundle\ConcertoTestBundle(),
-            new Cocur\Slugify\Bridge\Symfony\CocurSlugifyBundle(),
             new Concerto\APIBundle\ConcertoAPIBundle()
         ];
 
