59e696d9cf8e9f30dfc14850a4a1367d784b0862
Przemyslaw Lis <przemyslawlis@parabola-interactive.com>
2018-01-18 18:05:18 +0100

    update to symfony 3.1

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,6 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
 
-            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new FOS\OAuthServerBundle\FOSOAuthServerBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
             new Concerto\PanelBundle\ConcertoPanelBundle(),
