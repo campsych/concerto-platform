44c92c305f1b206126afc0185519a91fe46b3bb8
Przemyslaw Lis <przemyslawlis@parabola-interactive.com>
2018-03-06 12:02:41 +0100

    fixed R code autocompletion inside ports and connections on flow

--- src/Concerto/PanelBundle/Resources/public/angularjs/app/concerto_panel/js/controllers/connection_return_function_controller.js
+++ src/Concerto/PanelBundle/Resources/public/angularjs/app/concerto_panel/js/controllers/connection_return_function_controller.js
@@ -1,45 +1,55 @@
-function ConnectionReturnFunctionController($scope, $uibModalInstance, $timeout, RDocumentation, object, title, editable) {
-    $scope.object = object;
-    $scope.title = title;
-    $scope.editable = editable;
-    
-    $scope.codeOptions = {
-        lineWrapping: true,
-        lineNumbers: true,
-        mode: 'r',
-        viewportMargin: Infinity,
-        readOnly: !editable,
-        hintOptions: {
-            completeSingle: false,
-            wizardService: RDocumentation
-        },
-        extraKeys: {
-            "F11": function (cm) {
-                cm.setOption("fullScreen", !cm.getOption("fullScreen"));
-            },
-            "Esc": function (cm) {
-                if (cm.getOption("fullScreen"))
-                    cm.setOption("fullScreen", false);
-            },
-            "Ctrl-Space": "autocomplete"
-        }
-    };
+function ConnectionReturnFunctionController($scope, $uibModalInstance, $timeout, $http, RDocumentation, object, title, editable) {
+  $scope.object = object;
+  $scope.title = title;
+  $scope.editable = editable;
 
-    $scope.change = function () {
-        $scope.object.defaultReturnFunction = "0";
-        $uibModalInstance.close($scope.object);
-    };
-    
-    $scope.reset = function() {
-        $scope.object.defaultReturnFunction = "1";
-        $uibModalInstance.close($scope.object);
-    };
+  $scope.codeOptions = {
+    lineWrapping: true,
+    lineNumbers: true,
+    mode: 'r',
+    viewportMargin: Infinity,
+    readOnly: !editable,
+    hintOptions: {
+      completeSingle: false,
+      wizardService: RDocumentation
+    },
+    extraKeys: {
+      "F11": function (cm) {
+        cm.setOption("fullScreen", !cm.getOption("fullScreen"));
+      },
+      "Esc": function (cm) {
+        if (cm.getOption("fullScreen"))
+          cm.setOption("fullScreen", false);
+      },
+      "Ctrl-Space": "autocomplete"
+    }
+  };
+  if (RDocumentation.functionIndex === null) {
+    $http.get(RDocumentation.rCacheDirectory + 'functionIndex.json').success(function (data) {
+      if (data !== null) {
+        RDocumentation.functionIndex = data;
+        $scope.codeOptions.hintOptions.functionIndex = data;
+      }
+    });
+  } else {
+    $scope.codeOptions.hintOptions.functionIndex = RDocumentation.functionIndex;
+  }
 
-    $scope.cancel = function () {
-        $uibModalInstance.dismiss(0);
-    };
-    
-    $timeout(function () {
-        $scope.codemirrorForceRefresh++;
-    }, 20);
+  $scope.change = function () {
+    $scope.object.defaultReturnFunction = "0";
+    $uibModalInstance.close($scope.object);
+  };
+
+  $scope.reset = function () {
+    $scope.object.defaultReturnFunction = "1";
+    $uibModalInstance.close($scope.object);
+  };
+
+  $scope.cancel = function () {
+    $uibModalInstance.dismiss(0);
+  };
+
+  $timeout(function () {
+    $scope.codemirrorForceRefresh++;
+  }, 20);
 }
--- src/Concerto/PanelBundle/Resources/public/angularjs/app/concerto_panel/js/controllers/port_value_edit_controller.js
+++ src/Concerto/PanelBundle/Resources/public/angularjs/app/concerto_panel/js/controllers/port_value_edit_controller.js
@@ -1,44 +1,54 @@
-function PortValueEditController($scope, $uibModalInstance, $timeout, RDocumentation, object, editable) {
-    $scope.object = object;
-    $scope.editable = editable;
-    
-    $scope.codeOptions = {
-        lineWrapping: true,
-        lineNumbers: true,
-        mode: 'r',
-        viewportMargin: Infinity,
-        readOnly: !editable,
-        hintOptions: {
-            completeSingle: false,
-            wizardService: RDocumentation
-        },
-        extraKeys: {
-            "F11": function (cm) {
-                cm.setOption("fullScreen", !cm.getOption("fullScreen"));
-            },
-            "Esc": function (cm) {
-                if (cm.getOption("fullScreen"))
-                    cm.setOption("fullScreen", false);
-            },
-            "Ctrl-Space": "autocomplete"
-        }
-    };
+function PortValueEditController($scope, $uibModalInstance, $timeout, $http, RDocumentation, object, editable) {
+  $scope.object = object;
+  $scope.editable = editable;
 
-    $scope.change = function () {
-        $scope.object.defaultValue = "0";
-        $uibModalInstance.close($scope.object);
-    };
-    
-    $scope.reset = function() {
-        $scope.object.defaultValue = "1";
-        $uibModalInstance.close($scope.object);
-    };
+  $scope.codeOptions = {
+    lineWrapping: true,
+    lineNumbers: true,
+    mode: 'r',
+    viewportMargin: Infinity,
+    readOnly: !editable,
+    hintOptions: {
+      completeSingle: false,
+      wizardService: RDocumentation
+    },
+    extraKeys: {
+      "F11": function (cm) {
+        cm.setOption("fullScreen", !cm.getOption("fullScreen"));
+      },
+      "Esc": function (cm) {
+        if (cm.getOption("fullScreen"))
+          cm.setOption("fullScreen", false);
+      },
+      "Ctrl-Space": "autocomplete"
+    }
+  };
+  if (RDocumentation.functionIndex === null) {
+    $http.get(RDocumentation.rCacheDirectory + 'functionIndex.json').success(function (data) {
+      if (data !== null) {
+        RDocumentation.functionIndex = data;
+        $scope.codeOptions.hintOptions.functionIndex = data;
+      }
+    });
+  } else {
+    $scope.codeOptions.hintOptions.functionIndex = RDocumentation.functionIndex;
+  }
 
-    $scope.cancel = function () {
-        $uibModalInstance.dismiss(0);
-    };
+  $scope.change = function () {
+    $scope.object.defaultValue = "0";
+    $uibModalInstance.close($scope.object);
+  };
 
-    $timeout(function () {
-        $scope.codemirrorForceRefresh++;
-    }, 20);
+  $scope.reset = function () {
+    $scope.object.defaultValue = "1";
+    $uibModalInstance.close($scope.object);
+  };
+
+  $scope.cancel = function () {
+    $uibModalInstance.dismiss(0);
+  };
+
+  $timeout(function () {
+    $scope.codemirrorForceRefresh++;
+  }, 20);
 }
