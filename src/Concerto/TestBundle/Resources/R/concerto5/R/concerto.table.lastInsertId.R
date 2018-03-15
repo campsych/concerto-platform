0b4a3966396cd0d6eab35ee2da3ba19fbe0e5a52
Przemyslaw Lis <przemyslawlis@parabola-interactive.com>
2018-03-15 21:31:06 +0100

    db driver fix

--- src/Concerto/TestBundle/Resources/R/concerto5/R/concerto.table.lastInsertId.R
+++ src/Concerto/TestBundle/Resources/R/concerto5/R/concerto.table.lastInsertId.R
@@ -1,7 +1,7 @@
 concerto.table.lastInsertId <-
 function(){
   id = NULL
-  if(concerto$connection$driver == "pdo_sqlsrv") {
+  if(concerto$connectionParams$driver == "pdo_sqlsrv") {
     id = concerto$sqlsrv_last_insert_id
   } else {
     id = dbGetQuery(concerto$connection, "SELECT LAST_INSERT_ID();")[1,1]
