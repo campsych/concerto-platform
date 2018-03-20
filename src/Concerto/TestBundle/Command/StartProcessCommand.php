5f93b86d31270b2aa158f969406cb05747a48ab6
Przemyslaw Lis <przemyslawlis@parabola-interactive.com>
2018-03-20 18:41:49 +0100

    fork socket set to blocking on PHP side

--- src/Concerto/TestBundle/Command/StartProcessCommand.php
+++ src/Concerto/TestBundle/Command/StartProcessCommand.php
@@ -151,14 +151,10 @@ class StartProcessCommand extends Command
             return false;
         }
 
-        $retries = 0;
-        while (($success = @socket_connect($sock, gethostbyname("localhost"), 9099)) === false) {
-            $this->log(__FUNCTION__, "socket_connect() failed, response socket, " . socket_strerror(socket_last_error($sock)) . " (retry: $retries)");
-            $retries++;
-            usleep(200 * 1000);
-        }
-        if ($success === false) {
-            $this->log(__FUNCTION__, "socket_connect() failed, response socket, " . socket_strerror(socket_last_error($sock)) . " - aborting", true);
+        socket_set_block($sock);
+
+        if (@socket_connect($sock, gethostbyname("localhost"), 9099) === false) {
+            $this->log(__FUNCTION__, "socket_connect() failed, response socket, " . socket_strerror(socket_last_error($sock)), true);
             socket_close($sock);
             return false;
         }
