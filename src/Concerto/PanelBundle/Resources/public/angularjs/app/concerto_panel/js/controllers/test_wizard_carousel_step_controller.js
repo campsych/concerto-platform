3489bceaaebaf3e69cc3827fbd263c832afaa3fe
Przemyslaw Lis <przemyslawlis@parabola-interactive.com>
2018-07-23 16:02:23 +0200

    test wizard gui fix

--- src/Concerto/PanelBundle/Resources/public/angularjs/app/concerto_panel/js/controllers/test_wizard_carousel_step_controller.js
+++ src/Concerto/PanelBundle/Resources/public/angularjs/app/concerto_panel/js/controllers/test_wizard_carousel_step_controller.js
@@ -1,20 +1,24 @@
 function TestWizardCarouselStepController($scope, $http, $filter, TestWizardParam) {
 
-    $scope.savePath = Paths.TEST_WIZARD_PARAM_SAVE;
-    $scope.testWizardParamsService = TestWizardParam;
+  $scope.savePath = Paths.TEST_WIZARD_PARAM_SAVE;
+  $scope.testWizardParamsService = TestWizardParam;
 
+  $scope.values = {};
+
+  $scope.filterByGuiEligible = function (obj) {
+    return typeof(obj.exposed) === 'undefined' || obj.exposed == 0;
+  };
+
+  $scope.mapParamsValue = function () {
     $scope.values = {};
+    for (var i = 0; i < $scope.step.params.length; i++) {
+      var param = $scope.step.params[i];
+      TestWizardParam.unserializeParamValue(param);
+      $scope.values[param.name] = param.output;
+    }
+  };
 
-    $scope.mapParamsValue = function () {
-        $scope.values = {};
-        for (var i = 0; i < $scope.step.params.length; i++) {
-            var param = $scope.step.params[i];
-            TestWizardParam.unserializeParamValue(param);
-            $scope.values[param.name] = param.output;
-        }
-    };
-
-    $scope.mapParamsValue();
+  $scope.mapParamsValue();
 }
 
 concertoPanel.controller('TestWizardCarouselStepController', ["$scope", "$http", "$filter", "TestWizardParam", TestWizardCarouselStepController]);
