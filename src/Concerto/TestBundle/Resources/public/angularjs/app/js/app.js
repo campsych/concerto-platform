7fb0eb43e33e1a2074f90a59045f6b370a179338
Przemyslaw Lis <przemyslawlis@parabola-interactive.com>
2019-02-01 18:13:16 +0100

    exposed compileProvider

--- src/Concerto/TestBundle/Resources/public/angularjs/app/js/app.js
+++ src/Concerto/TestBundle/Resources/public/angularjs/app/js/app.js
@@ -1,46 +1,48 @@
 'use strict';
 
 var testRunner = angular.module('testRunner', [
-    'ngSanitize',
-    'angularFileUpload'
+  'ngSanitize',
+  'angularFileUpload'
 ]).config(function ($httpProvider) {
-    $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
-    var param = function (obj) {
-        var query = '', name, value, fullSubName, subName, subValue, innerObj, i;
+  $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
+  var param = function (obj) {
+    var query = '', name, value, fullSubName, subName, subValue, innerObj, i;
 
-        for (name in obj) {
-            value = obj[name];
+    for (name in obj) {
+      value = obj[name];
 
-            if (value instanceof Array) {
-                for (i = 0; i < value.length; ++i) {
-                    subValue = value[i];
-                    fullSubName = name + '[' + i + ']';
-                    innerObj = {};
-                    innerObj[fullSubName] = subValue;
-                    query += param(innerObj) + '&';
-                }
-            } else if (value instanceof Object) {
-                for (subName in value) {
-                    subValue = value[subName];
-                    fullSubName = name + '[' + subName + ']';
-                    innerObj = {};
-                    innerObj[fullSubName] = subValue;
-                    query += param(innerObj) + '&';
-                }
-            } else if (value !== undefined && value !== null)
-                query += encodeURIComponent(name) + '=' + encodeURIComponent(value) + '&';
+      if (value instanceof Array) {
+        for (i = 0; i < value.length; ++i) {
+          subValue = value[i];
+          fullSubName = name + '[' + i + ']';
+          innerObj = {};
+          innerObj[fullSubName] = subValue;
+          query += param(innerObj) + '&';
         }
-
-        return query.length ? query.substr(0, query.length - 1) : query;
-    };
-
-    $httpProvider.defaults.transformRequest = [
-        function (data) {
-            return angular.isObject(data) && String(data) !== '[object File]' ? param(data) : data;
+      } else if (value instanceof Object) {
+        for (subName in value) {
+          subValue = value[subName];
+          fullSubName = name + '[' + subName + ']';
+          innerObj = {};
+          innerObj[fullSubName] = subValue;
+          query += param(innerObj) + '&';
         }
-    ];
+      } else if (value !== undefined && value !== null)
+        query += encodeURIComponent(name) + '=' + encodeURIComponent(value) + '&';
+    }
+
+    return query.length ? query.substr(0, query.length - 1) : query;
+  };
+
+  $httpProvider.defaults.transformRequest = [
+    function (data) {
+      return angular.isObject(data) && String(data) !== '[object File]' ? param(data) : data;
+    }
+  ];
 }).config(function ($controllerProvider) {
-    testRunner.controllerProvider = $controllerProvider;
+  testRunner.controllerProvider = $controllerProvider;
 }).config(function ($interpolateProvider) {
-    $interpolateProvider.startSymbol('[[').endSymbol(']]');
+  $interpolateProvider.startSymbol('[[').endSymbol(']]');
+}).config(function ($compileProvider) {
+  testRunner.compileProvider = $compileProvider;
 });
