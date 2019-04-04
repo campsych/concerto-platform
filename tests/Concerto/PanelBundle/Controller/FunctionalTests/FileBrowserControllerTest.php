0da3d4bbd556aec588ede7041baeca8a85ba5ddd
Przemyslaw Lis <przemyslawlis@parabola-interactive.com>
2019-04-04 19:08:17 +0200

    travis

--- tests/Concerto/PanelBundle/Controller/FunctionalTests/FileBrowserControllerTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Tests\Concerto\PanelBundle\Controller\FunctionalTests;
-
-use Tests\Concerto\PanelBundle\AFunctionalTest;
-
-class FileBrowserControllerTest extends AFunctionalTest {
-
-}
