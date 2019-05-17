8337e0cb7c90f66ccc56a027836d59a041a17250
Bartosz Kielczewski <b.kielczewski@jbs.cam.ac.uk>
2019-05-17 12:03:05 +0100

    Removed Rancher deployment config

--- deploy/rancher/install.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-sudo ros service enable https://raw.githubusercontent.com/campsych/concerto-platform/master/deploy/rancher/concerto.yml
-sudo ros service up database
-sudo ros service up concerto
