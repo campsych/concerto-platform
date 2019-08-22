b8a10bab83ba89a559c0e6e567205cd66becd5fa
Przemyslaw Lis <przemyslawlis@parabola-interactive.com>
2019-08-22 17:59:07 +0200

    fixed handling of non numeric values of timeLimit in showPage

--- src/Concerto/PanelBundle/Resources/starter_content/src/test/_showPage/_showPage.r
+++ src/Concerto/PanelBundle/Resources/starter_content/src/test/_showPage/_showPage.r
@@ -8,11 +8,12 @@ getTemplateParams = function(templateParams) {
   return(templateParams)
 }
 
+numericTimeLimit = as.numeric(timeLimit)
 response = concerto5:::concerto.template.show(
   template=template, 
   html=if(!is.null(html) && !is.na(html) && html != "") {html} else {""},
   params=getTemplateParams(templateParams), 
-  timeLimit=if(!is.na(timeLimit)) { as.numeric(timeLimit) } else { 0 }
+  timeLimit=if(!is.na(numericTimeLimit)) { numericTimeLimit } else { 0 }
 )
 
 for(.name in .dynamicReturns) {
