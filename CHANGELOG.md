91f97591cd2c13176aba6532d3d5e482c58cb4a8
Przemyslaw Lis <przemyslawlis@parabola-interactive.com>
2019-09-09 18:53:34 +0200

    added insertId return variable to dataManipulation node refixed local variables in _dataManipulation to prevent name overlaps with dynamic inputs fixed data manipulation wizard descriptions and param's hide rules

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,12 +1,15 @@
-## 5.0.1
+## 5.0.1 (2019-09-09)
 
 #### Starter content
 * added **log** node
 * added wizard for **eval** node
 * **showPage** now has native **.branch** return port and will use **buttonPressed** value for **.branch** when at least one dynamic branch is created
+* added **insertId** return variable to **dataManipulation** node
 
 #### Fixes
 * fixed disappearing values on some wizard params
 * fixed complex type wizard param's nesting
 * fixed **linearTest** custom item table
 * fixed setting null data table wizard param related values to recently edited data table
+* prefixed local variables in **_dataManipulation** to prevent name overlaps with dynamic inputs
+* fixed data manipulation wizard descriptions and param's hide rules
--- src/Concerto/PanelBundle/Resources/starter_content/src/test/_dataManipulation/_dataManipulation.r
+++ src/Concerto/PanelBundle/Resources/starter_content/src/test/_dataManipulation/_dataManipulation.r
@@ -1,123 +1,125 @@
-getWhereClause = function(where, type) {
-  if((is.na(where) || where == "") && type != "update") {
-    elems = NULL
-    for(name in .dynamicInputs) {
-      elem = concerto.table.insertParams("{{name}}='{{value}}'", list(
-        name=name,
-        value=get(name)
+getWhereClause = function(whereClause, queryType) {
+  if((is.na(whereClause) || whereClause == "") && queryType != "update") {
+    .elems = NULL
+    for(.name in .dynamicInputs) {
+      .elem = concerto.table.insertParams("{{name}}='{{value}}'", list(
+        name=.name,
+        value=get(.name)
       ))
-      elems = c(elems, elem)
+      .elems = c(.elems, .elem)
     }
-    if(length(elems) > 0) {
-      elemsJoined = paste(elems, collapse=" AND ")
-      return(paste0("WHERE ", elemsJoined))
+    if(length(.elems) > 0) {
+      .elemsJoined = paste(.elems, collapse=" AND ")
+      return(paste0("WHERE ", .elemsJoined))
     }
-  } else if(length(where) == 1) {
-    id = suppressWarnings(as.numeric(where))
-    if(is.character(where) && is.na(id)) {
-      params = list()
-      for(name in .dynamicInputs) {
-        params[[name]] = get(name)
+  } else if(length(whereClause) == 1) {
+    .id = suppressWarnings(as.numeric(whereClause))
+    if(is.character(whereClause) && is.na(.id)) {
+      .params = list()
+      for(.name in .dynamicInputs) {
+        .params[[.name]] = get(.name)
       }
-      elemsJoined = concerto.table.insertParams(where, params)
-      return(paste0("WHERE ", elemsJoined))
-    } else if(is.numeric(id)) {
-      return(paste0("WHERE id=",where))
+      .elemsJoined = concerto.table.insertParams(whereClause, .params)
+      if(.elemsJoined == "") { return(NULL) }
+      return(paste0("WHERE ", .elemsJoined))
+    } else if(is.numeric(.id)) {
+      return(paste0("WHERE id=", whereClause))
     }
   }
   return(NULL)
 }
     
-getSetClause = function(set, type) {
-  if(is.na(set) || set == "") {
-    if(type == "insert") {
-      cols = NULL
-      vals = NULL
-      for(name in .dynamicInputs) {
-        cols = c(cols, name)
-        val = concerto.table.insertParams("'{{value}}'", list(
-          value=get(name)
+getSetClause = function(setClause, queryType) {
+  if(is.na(setClause) || setClause == "") {
+    if(queryType == "insert") {
+      .cols = NULL
+      .vals = NULL
+      for(.name in .dynamicInputs) {
+        .cols = c(.cols, .name)
+        .val = concerto.table.insertParams("'{{value}}'", list(
+          value=get(.name)
         ))
-        vals = c(vals, val)
+        .vals = c(.vals, .val)
       }
-      if(length(cols) > 0) {
-        colsJoined = paste(cols, collapse=", ")
-        valsJoined = paste(vals, collapse=", ")
-        return(paste0("(",colsJoined,") VALUES (",valsJoined,")"))
+      if(length(.cols) > 0) {
+        .colsJoined = paste(.cols, collapse=", ")
+        .valsJoined = paste(.vals, collapse=", ")
+        return(paste0("(", .colsJoined, ") VALUES (", .valsJoined, ")"))
       }
     } else {
-      elems = NULL
-      for(name in .dynamicInputs) {
-        elem = concerto.table.insertParams("{{name}}='{{value}}'", list(
-          name=name,
-          value=get(name)
+      .elems = NULL
+      for(.name in .dynamicInputs) {
+        .elem = concerto.table.insertParams("{{name}}='{{value}}'", list(
+          name=.name,
+          value=get(.name)
         ))
-        elems = c(elems, elem)
+        .elems = c(.elems, .elem)
       }
-      if(length(elems) > 0) {
-        elemsJoined = paste(elems, collapse=", ")
-        return(paste0("SET ", elemsJoined))
+      if(length(.elems) > 0) {
+        .elemsJoined = paste(.elems, collapse=", ")
+        return(paste0("SET ", .elemsJoined))
       }
     }
-  } else if(length(set) == 1 && is.character(set)) {
-    params = list()
-    for(name in .dynamicInputs) {
-      params[[name]] = get(name)
+  } else if(length(setClause) == 1 && is.character(setClause)) {
+    .params = list()
+    for(.name in .dynamicInputs) {
+      .params[[.name]] = get(.name)
     }
-    elemsJoined = concerto.table.insertParams(set, params)
-    return(paste0("SET ", elemsJoined))
+    .elemsJoined = concerto.table.insertParams(setClause, .params)
+    return(paste0("SET ", .elemsJoined))
   }
   return(NULL)
 }
   
 getCustomQuery = function(queryString) {
-  params = list()
-  for(name in .dynamicInputs) {
-    params[[name]] = get(name)
+  .params = list()
+  for(.name in .dynamicInputs) {
+    .params[[.name]] = get(.name)
   }
-  queryString = concerto.table.insertParams(queryString, params)
+  queryString = concerto.table.insertParams(queryString, .params)
   return(queryString)
 }
 
 result = NULL
 if(queryType == "select") {
   queryString = "SELECT * FROM {{table}}"
-  whereString = getWhereClause(whereClause, queryType)
-  if(!is.null(whereString)) {
-    queryString = paste0(queryString, " ", whereString)
+  .whereString = getWhereClause(whereClause, queryType)
+  if(!is.null(.whereString)) {
+    queryString = paste0(queryString, " ", .whereString)
   }
   result = concerto.table.query(queryString, params=list(
     table=table
   ))
 } else if(queryType == "insert") {
   queryString = "INSERT INTO {{table}}"
-  setString = getSetClause(setClause, queryType)
-  if(!is.null(setString)) {
-    queryString = paste0(queryString, " ", setString)
+  .setString = getSetClause(setClause, queryType)
+  if(!is.null(.setString)) {
+    queryString = paste0(queryString, " ", .setString)
   }
   result = concerto.table.query(queryString, params=list(
     table=table
   ))
+  insertId = concerto.table.lastInsertId()
 } else if(queryType == "update") {
   queryString = "UPDATE {{table}}"
-  setString = getSetClause(setClause, queryType)
-  if(!is.null(setString)) {
-    queryString = paste0(queryString, " ", setString)
+  .setString = getSetClause(setClause, queryType)
+  if(!is.null(.setString)) {
+    queryString = paste0(queryString, " ", .setString)
   } else {
     stop("No 'set' clause for UPDATE query")
   }
-  whereString = getWhereClause(whereClause, queryType)
-  if(!is.null(whereString)) {
-    queryString = paste0(queryString, " ", whereString)
+  .whereString = getWhereClause(whereClause, queryType)
+  if(!is.null(.whereString)) {
+    queryString = paste0(queryString, " ", .whereString)
   }
   result = concerto.table.query(queryString, params=list(
     table=table
   ))
 } else if(queryType == "delete") {
   queryString = "DELETE FROM {{table}}"
-  whereString = getWhereClause(whereClause, queryType)
-  if(!is.null(whereString)) {
-    queryString = paste0(queryString, " ", whereString)
+  .whereString = getWhereClause(whereClause, queryType)
+  if(!is.null(.whereString)) {
+    queryString = paste0(queryString, " ", .whereString)
   }
   result = concerto.table.query(queryString, params=list(
     table=table
