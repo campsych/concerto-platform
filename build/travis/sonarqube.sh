e95cec3e5ad3ab97fab542652db1b0399b7f27f5
Bartosz Kielczewski <bartosz@kielczewski.eu>
2021-02-23 14:30:11 +0000

    Remove unneeded env when invoking sonar scanner

--- build/travis/sonarqube.sh
+++ build/travis/sonarqube.sh
@@ -4,7 +4,6 @@ if [[ -n $TRAVIS_TAG ]]
 then
   docker run \
       --rm \
-      -e SONAR_PROJECT_KEY="concerto-platform" \
       -e SONAR_HOST_URL="${SONAR_HOST_URL}" \
       -e SONAR_LOGIN="${SONAR_LOGIN}" \
       -v "${TRAVIS_BUILD_DIR}:/usr/src" \
