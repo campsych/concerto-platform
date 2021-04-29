f1458c42213b9c5bfc3262d44698085d6d6aa3b1
Przemyslaw Lis <przemyslawlis@parabola-interactive.com>
2021-04-29 16:04:04 +0200

    changed google authenticator name

--- src/Concerto/PanelBundle/Entity/User.php
+++ src/Concerto/PanelBundle/Entity/User.php
@@ -378,7 +378,7 @@ class User extends ATopEntity implements AdvancedUserInterface, \Serializable, \
 
     public function getGoogleAuthenticatorUsername(): string
     {
-        return "Concerto Platform (" . $this->username . "@" . $_SERVER["HTTP_HOST"] . ")";
+        return "Concerto (" . $this->username . "@" . $_SERVER["HTTP_HOST"] . ")";
     }
 
     public function getGoogleAuthenticatorSecret(): ?string
