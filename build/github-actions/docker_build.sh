29782d4e082ac9d711db47f84f2dfe1700e58e52
przemyslaw_lis <pl362@cam.ac.uk>
2024-03-20 13:06:27 +0100

    arm64v8 image

--- build/github-actions/docker_build.sh
+++ build/github-actions/docker_build.sh
@@ -4,7 +4,7 @@ PLATFORM="linux/arm64"
 
 if [[ -n $GITHUB_REF_NAME ]]
 then
-  docker buildx build --platform $PLATFORM -t campsych/concerto-platform:$GITHUB_REF_NAME .
+  docker buildx build --platform $PLATFORM --load -t campsych/concerto-platform:$GITHUB_REF_NAME .
   docker tag campsych/concerto-platform:$GITHUB_REF_NAME campsych/concerto-platform:test
   if [[ $GITHUB_REF_NAME = master ]]; then docker tag campsych/concerto-platform:$GITHUB_REF_NAME campsych/concerto-platform:latest; fi
 fi
@@ -12,7 +12,7 @@ fi
 if [[ -n $GITHUB_SHA ]]
 then
   COMMIT_SHORT=${GITHUB_SHA:0:7}
-  docker buildx build --platform $PLATFORM -t campsych/concerto-platform:$COMMIT_SHORT .
+  docker buildx build --platform $PLATFORM --load -t campsych/concerto-platform:$COMMIT_SHORT .
   docker tag campsych/concerto-platform:$COMMIT_SHORT campsych/concerto-platform:test
 fi
 
@@ -20,10 +20,10 @@ if [[ -n $GITHUB_REF_NAME ]]
 then
   if [[ $GITHUB_REF_NAME =~ ^(v)?([0-9]+)\.([0-9]+)\.([0-9]+)$ ]]
   then
-    docker buildx build --platform $PLATFORM -t campsych/concerto-platform:${BASH_REMATCH[1]}${BASH_REMATCH[2]} .
+    docker buildx build --platform $PLATFORM --load -t campsych/concerto-platform:${BASH_REMATCH[1]}${BASH_REMATCH[2]} .
     docker tag campsych/concerto-platform:${BASH_REMATCH[1]}${BASH_REMATCH[2]}
 
-    docker buildx build --platform $PLATFORM -t campsych/concerto-platform:${BASH_REMATCH[1]}${BASH_REMATCH[2]}.${BASH_REMATCH[3]} .
+    docker buildx build --platform $PLATFORM --load -t campsych/concerto-platform:${BASH_REMATCH[1]}${BASH_REMATCH[2]}.${BASH_REMATCH[3]} .
     docker tag campsych/concerto-platform:${BASH_REMATCH[1]}${BASH_REMATCH[2]}.${BASH_REMATCH[3]}
   fi
 fi
